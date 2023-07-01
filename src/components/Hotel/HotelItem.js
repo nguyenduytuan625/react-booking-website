0d9b4b78fb23655c1e3d84db44aeb9e47808ff35
nguyenduytuan625 <nguyenduytuan1600pc@gmail.com>
2023-07-01 14:38:12 +0700

    ninteenth commit

--- src/components/Hotel/HotelItem.js
+++ src/components/Hotel/HotelItem.js
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import styles from './HotelItem.module.css';
 
 const HotelItem = props => {
   return (
     <div className={styles.hotelitem}>
       <img src={props.image_url} />
-      <a className={styles['mb-1']} href='/detail' >{props.name}</a>
+      <Link className={styles['mb-1']} to='/detail' >{props.name}</Link>
       <p className={styles['mb-1']}>{props.city}</p>
       <p className={styles['mb-1']}><b>Starting from ${props.price}</b></p>
       <p className={styles['mb-1']}><span>{props.rate}</span> {props.type}</p>
