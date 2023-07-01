93c3191a7c3c4c55cac7a2d15045131b15e7a6d7
nguyenduytuan625 <nguyenduytuan1600pc@gmail.com>
2023-07-01 13:57:58 +0700

    eleventh commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Search from "./pages/search/Search";
 function App() {
   return (
     // <BrowserRouter>
-    <HashRouter basename="/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
